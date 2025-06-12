3a87c4b42283ca85525eb97427f472b73e327fe9
Vitaliy <vitaliysamborsky2002@gmail.com>
2025-06-12 20:04:14 +0300

    +feat: add header component

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,16 +1,12 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import { RouterApp } from '../enum/enumRouter.ts';
+import { Header } from '../components/header.tsx';
+// import { RouterApp } from '../enum/enumRouter.ts';
 
 const Layout = () => {
     return (
         <>
-            <div>
-                <h1>Дім</h1>
-                <Link to={RouterApp.signUp}>A</Link>
-                <Link to={RouterApp.signIn}>B</Link>
-            </div>
-
+            <Header />
             <Outlet />
         </>
     );
